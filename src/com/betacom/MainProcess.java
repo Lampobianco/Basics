624a77498dcf572f9227118068e370eea4026b65
Lampobianco <daniele.lometti@gmail.com>
2026-05-20 21:04:57 +0200

    es. "Aggiunta classi Lombok, Inner, Builder

--- src/com/betacom/MainProcess.java
+++ src/com/betacom/MainProcess.java
@@ -7,14 +7,18 @@ import com.betacom.exeption.AcademyExeption;
 import com.betacom.interfaces.GeneralProcess;
 import com.betacom.processes.AnonimusManager;
 import com.betacom.processes.BaseManager;
+import com.betacom.processes.BuilderManager;
 import com.betacom.processes.CarManager;
 import com.betacom.processes.CollectionManager;
 import com.betacom.processes.DateManager;
 import com.betacom.processes.EnumManager;
 import com.betacom.processes.ExeptionManager;
+import com.betacom.processes.InnerManager;
 import com.betacom.processes.InterfacesManager;
 import com.betacom.processes.JsonManager;
 import com.betacom.processes.ListManager;
+import com.betacom.processes.LogManager;
+import com.betacom.processes.LombokManager;
 import com.betacom.processes.MapManager;
 import com.betacom.processes.SequenzialManager;
 import com.betacom.processes.StreamManager;
@@ -36,7 +40,11 @@ public class MainProcess {
 			Map.entry("sequenzial", new SequenzialManager()),
 			Map.entry("stream",     new StreamManager()),
 			Map.entry("anonimus",   new AnonimusManager()),
-			Map.entry("json",       new JsonManager())
+			Map.entry("json",       new JsonManager()),
+			Map.entry("inner",      new InnerManager()),
+			Map.entry("builder",      new BuilderManager()),
+			Map.entry("lombok",      new LombokManager()),
+			Map.entry("log",         new LogManager())
 	);
 
 	public static void main(String[] args) {
